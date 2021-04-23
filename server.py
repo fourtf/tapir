8f5117d8162b2617edaa4885fea9eb6d0d7e08bd
fourtf <tf.four@gmail.com>
2021-04-23 18:46:47 +0200

    removed some unused code from server.py

--- server.py
+++ server.py
@@ -10,24 +10,12 @@ class S(BaseHTTPRequestHandler):
         self.send_header("Content-type", "text/html")
         self.end_headers()
 
-    def _html(self, message):
-        """This just generates an HTML document that includes `message`
-        in the body. Override, or re-write this do do more interesting stuff.
-        """
-        content = f"<html><body><h1>{message}</h1></body></html>"
-        return content.encode("utf8")  # NOTE: must return a bytes object!
-
     def do_GET(self):
         self._set_headers()
         self.wfile.write(read_file("./index.html"))
 
     def do_HEAD(self):
         self._set_headers()
-
-    def do_POST(self):
-        # Doesn't do anything with posted data
-        self._set_headers()
-        self.wfile.write(self._html("POST!"))
 
 
 def run(addr, port, server_class=HTTPServer):
